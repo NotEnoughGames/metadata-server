bece7b79af1b310b2efbfb32e15a9d8f2235ea78
oott123 <git@public.oott123.com>
2018-04-15 14:55:18 +0800

    add cache

--- src/routes/meta.js
+++ src/routes/meta.js
@@ -10,6 +10,9 @@ import { getSingleAppById, getAppsByIds } from '../models/app'
 export default function(app) {
   app.use(
     Route.get('/app/:appId', (ctx, appId) => {
+      // 尽管我们设置了一周的缓存，但由于 CORS 的存在，Vary 头被设置为了 Origin。
+      // 这意味着不同的 Origin 会有不同的缓存。
+      ctx.response.set('Cache-Control', `public, max-age=${60 * 60 * 24 * 7}`)
       return getSingleAppById(appId)
     })
   )
